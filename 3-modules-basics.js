c4e6f9cbfe8663cec1be33c1efadebf9e8186cdd
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-09-07 01:52:08 +0530

    importing invoked function gotcha

--- 3-modules-basics.js
+++ 3-modules-basics.js
@@ -5,10 +5,13 @@ const people = require('./4-names-module-setup');
 const sayHi = require('./5-utils-module-setup');
 const { singlePerson, items } = require('./6-alternative-export-syntax');
 
-// console.log(names);
+require('./7-function-import-gotcha');
+// ! this immediately runs the function because when we import a module we basically invoke it.
 
-sayHi(people.john);
-sayHi('susan');
-sayHi(people.peter);
+// // console.log(names);
 
-console.log(singlePerson, items);
+// sayHi(people.john);
+// sayHi('susan');
+// sayHi(people.peter);
+
+// console.log(singlePerson, items);
---
+++ 7-function-import-gotcha.js
@@ -0,0 +1,8 @@
+const num1 = 7;
+const num2 = 11;
+
+const addNum = (n1, n2) => {
+  console.log(n1 + n2);
+};
+
+addNum(num1, num2);
