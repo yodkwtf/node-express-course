f6b5d8eb378d354fbbb8533b97994340091da385
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-09-07 03:46:33 +0530

    http module - using req object for more features

--- 12-http-module.js
+++ 12-http-module.js
@@ -2,8 +2,19 @@ const http = require('http');
 
 // create a server
 const server = http.createServer((req, res) => {
-  res.write('welcome to our homepage');
-  res.end();
+  // use req object
+  if (req.url === '/') {
+    res.end('Welcome to the homepage!');
+  }
+
+  if (req.url === '/about') {
+    res.end('This is the about page');
+  }
+
+  res.end(`
+  <h1 style="color:red">OOPS!</h1>
+  <p>This page doesn't exist</p>
+  `);
 });
 
 // specify the port to run the server
