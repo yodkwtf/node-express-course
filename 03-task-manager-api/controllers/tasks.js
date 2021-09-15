d29792ddad0d659b9509f6fd5e0294c6acf77e08
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-09-15 17:19:50 +0530

    all routes & controllers setup

--- 03-task-manager-api/controllers/tasks.js
+++ 03-task-manager-api/controllers/tasks.js
@@ -1,7 +1,32 @@
+// # GET ALL TASKS
 const getAllTasks = (req, res) => {
-  res.send('all tasks');
+  res.send('get all tasks');
+};
+
+// # CREATE A TASK
+const createTask = (req, res) => {
+  res.send('create task');
+};
+
+// # GET A SINGLE TASK
+const getTask = (req, res) => {
+  res.send('get single task');
+};
+
+// # UPDATE A TASK
+const updateTask = (req, res) => {
+  res.send('update task');
+};
+
+// # DELETE A TASK
+const deleteTask = (req, res) => {
+  res.send('delete task');
 };
 
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
--- 03-task-manager-api/routes/tasks.js
+++ 03-task-manager-api/routes/tasks.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
 // controllers
-const { getAllTasks } = require('../controllers/tasks');
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require('../controllers/tasks');
 
 // # GET ALL TASKS
-// router.get('/', getAllTasks);
-router.route('/').get(getAllTasks);
+router.get('/', getAllTasks);
+
+// # CREATE A TASK
+router.post('/', createTask);
+
+// # GET A SINGLE TASK
+router.get('/:id', getTask);
+
+// # UPDATE A TASK
+router.patch('/:id', updateTask);
+
+// # DELETE A TASK
+router.delete(':/id', deleteTask);
 
 module.exports = router;
