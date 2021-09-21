c3c235dfdabbe191d2c4b41d5257796fab910449
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-09-21 19:02:56 +0530

    catching validations error w try-catch

--- 03-task-manager-api/controllers/tasks.js
+++ 03-task-manager-api/controllers/tasks.js
@@ -7,8 +7,12 @@ const getAllTasks = (req, res) => {
 
 // # CREATE A TASK
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 // # GET A SINGLE TASK
