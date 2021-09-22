0dfb40f847a58456af249ae1e9f1b38507e9f2de
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-09-23 04:00:38 +0530

    setting up the sort functionality

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,17 +1,13 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab';
-
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  const products = await Product.find({}).sort('name -price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   // get only properties u want
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   // setting up query object for the queries
   const queryObject = {};
@@ -26,9 +22,20 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' }; // means case insenstivity (search feature)
   }
 
-  console.log(queryObject);
-  // get products
-  const products = await Product.find(queryObject);
+  // console.log(queryObject);
+  // get products queries
+  let result = Product.find(queryObject);
+
+  // sort products
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  // get sorted products
+  const products = await result;
 
   // send products
   res.status(200).json({ products, nbHits: products.length });
--- 04-store-api/readme.md
+++ 04-store-api/readme.md
@@ -7,9 +7,10 @@
 
 - `express-async-errors` - removes the need of creating an async wrapper middleware to wrap the trycatch block of all the controllers and does all the heavy lifting itself
 
-### Filters
+### Filters & Sort
 
 - Getting all products
 - Based on featured products [true, false]
 - Based on product company name [single type options]
 - Based on product name [using mongoose regex pattern]
+- Sorting products based on properties
