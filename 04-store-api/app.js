7b6b88cf94146e2a2e44b4338397a33644f46234
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-09-23 01:48:54 +0530

    router setup

--- 04-store-api/app.js
+++ 04-store-api/app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -14,6 +15,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+// base path
+app.use('/api/v1/products', productsRouter);
 
 // products route
 
