3d3756e99b131bcc13c2889c97ed4078bd036744
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-09-23 02:59:52 +0530

    basic find to get the specific products

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,6 +1,8 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing aysnc-errors');
-  res.status(200).json({ msg: 'products testing route' });
+  const products = await Product.find({ name: 'vase table' });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
