e494a73479c43b8ccb79b10c52d47232601e5e0a
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-01 16:36:24 +0530

    creating and sending hwt token

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,15 +1,20 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // # REGISTER
 const register = async (req, res) => {
   // create user
   const user = await User.create({ ...req.body });
 
-  // send response
-  res.status(StatusCodes.CREATED).json({ user });
+  // create a jwt token
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+
+  // send token and user name as response [we can also decode name and other things in frontend at times]
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 // # LOGIN
