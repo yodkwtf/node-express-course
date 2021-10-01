dbc57e559217b0083c7df387eff545b9dbd70096
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-01 08:34:17 +0530

    hashing user password

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,6 +1,25 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const brcypt = require('bcryptjs');
+
 // # REGISTER
 const register = async (req, res) => {
-  res.send('register user');
+  // get user data
+  const { name, email, password } = req.body;
+
+  // no. of random bytes
+  const salt = await brcypt.genSalt(10);
+  // hash password
+  const hashedPassword = await brcypt.hash(password, salt);
+
+  // create a temp user with hashed password
+  const tempUser = { name, email, password: hashedPassword };
+
+  // create user
+  const user = await User.create({ ...tempUser });
+  console.log(user);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 // # LOGIN
--- 06-jobs-api/models/User.js
+++ 06-jobs-api/models/User.js
@@ -23,7 +23,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 6,
-    maxlength: 12,
   },
 });
 
