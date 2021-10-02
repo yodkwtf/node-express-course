dae1d43fbca2e34352c245ee5bf5620889a62550
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-03 02:50:59 +0530

    security packages setup

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -18,9 +24,19 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middlewares
+app.set('trust proxy', 1); // essential for heroku like setups
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100, // 15 mins
+  })
+);
 app.use(express.json());
 
-// extra packages
+// security packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // base url for router
 app.use('/api/v1/auth', authRouter);
