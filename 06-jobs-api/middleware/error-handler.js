2e7d268e008b9863acf8184cfab7fca14314e14d
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-03 02:22:43 +0530

    validation error setup

--- 06-jobs-api/middleware/error-handler.js
+++ 06-jobs-api/middleware/error-handler.js
@@ -1,18 +1,21 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || 'Something went wring try again later',
+    msg: err.message || 'Something went wrong try again later',
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // if there is a validation error [not provided password/email]
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+    customError.statusCode = 400;
   }
 
-  // if registering user already exists
+  // if registering user already exists [duplicate email]
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
@@ -20,7 +23,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = 400;
   }
 
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
