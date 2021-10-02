46163982488c4882eee7d7c9089b098d1e81dd4e
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-03 05:08:07 +0530

    API documentation added

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -38,6 +43,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+// front page
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // base url for router
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
