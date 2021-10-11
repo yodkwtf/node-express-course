2d6a4d1d2b2f7e162d56773728083c2e83b13c98
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-12 03:02:11 +0530

    register user controller - basic

--- 10-e-commerce-api/controllers/authController.js
+++ 10-e-commerce-api/controllers/authController.js
@@ -1,6 +1,14 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 // # REGISTER
 const register = async (req, res) => {
-  res.send('the REGISTER controller');
+  // create a user
+  const user = await User.create(req.body);
+
+  // send back the user as a response
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 // # LOGIN
