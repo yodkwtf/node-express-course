410553886bf9be73766649d44c7476f89fdcb14d
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-12 01:06:58 +0530

    basic express server

--- 10-e-commerce-api/app.js
+++ 10-e-commerce-api/app.js
@@ -1,1 +1,17 @@
-console.log('E-Commerce API');
+// express
+const express = require('express');
+const app = express();
+
+// port
+const port = process.env.PORT || 5000;
+
+// start
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
