ca3eec61f7b6c0ce208a811f56d5ac149057f67d
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-12 01:15:15 +0530

    connected to database

--- 10-e-commerce-api/app.js
+++ 10-e-commerce-api/app.js
@@ -1,13 +1,17 @@
+// dotenv
+require('dotenv').config();
 // express
 const express = require('express');
 const app = express();
+// database
+const connectDB = require('./db/connect');
 
 // port
 const port = process.env.PORT || 5000;
-
 // start
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
