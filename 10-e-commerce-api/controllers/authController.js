0d2bd881d19815faf816f6debf9c23ac44bc5c29
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-12 03:14:51 +0530

    register user - user roles set up

--- 10-e-commerce-api/controllers/authController.js
+++ 10-e-commerce-api/controllers/authController.js
@@ -5,7 +5,7 @@ const CustomError = require('../errors');
 // # REGISTER
 const register = async (req, res) => {
   // get the email
-  const { email } = req.body;
+  const { name, password, email } = req.body;
 
   // find a user by email entered
   const emailAlreadyExists = await User.findOne({ email });
@@ -15,8 +15,12 @@ const register = async (req, res) => {
     throw new CustomError.BadRequestError('Email already exists');
   }
 
-  // create a user
-  const user = await User.create(req.body);
+  // set up first account as admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+
+  // create a user with specific properties
+  const user = await User.create({ name, email, password, role });
 
   // send back the user as a response
   res.status(StatusCodes.CREATED).json({ user });
