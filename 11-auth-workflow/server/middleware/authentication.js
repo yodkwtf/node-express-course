981055f76f0d22ea4cb3bffe08f9787b57822c79
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-29 00:18:16 +0530

    auth middleware - checking for accesstoken

--- 11-auth-workflow/server/middleware/authentication.js
+++ 11-auth-workflow/server/middleware/authentication.js
@@ -2,16 +2,16 @@ const CustomError = require('../errors');
 const { isTokenValid } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
-  const token = req.signedCookies.token;
-
-  if (!token) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
-  }
+  // get both the tokens
+  const { accessToken, refreshToken } = req.signedCookies;
 
   try {
-    const { name, userId, role } = isTokenValid({ token });
-    req.user = { name, userId, role };
-    next();
+    // check is accessToken exists
+    if (accessToken) {
+      const payload = isTokenValid(accessToken);
+      req.user = payload.user;
+      return next();
+    }
   } catch (error) {
     throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
